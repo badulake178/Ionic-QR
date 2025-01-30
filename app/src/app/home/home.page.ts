880c7a9e78a376b27ac9d600c2b66c038f315e5d
Eduardo Fuentealba <eduardo.fuentealbau@gmail.com>
2025-01-30 16:44:51 -0300

    Guarda cambios

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import html2canvas from 'html2canvas';
-import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,14 +15,19 @@ export class HomePage {
   segment  = 'scan';
   qrText = '';
 
-  constructor() {}
+  constructor(
+    private loadingController: LoadingController,
+    private platform: Platform
+  ) {}
 
   // ===== Capture HTML element, convert it to canvas and get an iamge =====
   captureScreen() {
     const element = document.getElementById('qrImage') as HTMLElement;
 
     html2canvas(element).then((canvas: HTMLCanvasElement)=>{
-      this.downloadImage(canvas);
+
+      if(this.platform.is('capacitor')) this.downloadImage(canvas);
+      else this.downloadImage(canvas);
     })
 
   }
@@ -39,12 +46,31 @@ export class HomePage {
     let base64 = canvas.toDataURL();
     let path = 'qr.png';
 
+    const loading = await this.loadingController.create({spinner: 'crescent', });
+    await loading.present();
+
     await Filesystem.writeFile({
-      path: 'secrets/text.txt',
-      data: 'This is a test',
-      directory: Directory.Documents,
-      encoding: Encoding.UTF8,
+      path,
+      data: base64,
+      directory: Directory.Documents
+    }).then(async (res)=>{
+      let uri = res.uri;
+      await Share.share({
+        url: uri
+      });
+
+      await Filesystem.deleteFile({
+        path,
+        directory: Directory.Cache
+      })
+    }).finally(()=>{
+
+      loading.dismiss();
     });
+
+
+
+
 
 
   }
