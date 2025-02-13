bdbe8e502efca12922d7c68729878caf863daa25
Eduardo Fuentealba <eduardo.fuentealbau@gmail.com>
2025-02-13 13:06:06 -0300

    Guardar cambios

--- task-app-ionic/src/app/pages/tabs/home/home.page.ts
+++ task-app-ionic/src/app/pages/tabs/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
 
 @Component({
   standalone: false,
@@ -7,6 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  tasks: Task[] = [
+    {
+      id: "1",
+      title: 'Autenticacion con google',
+      description: 'Esta es una tarea de autenticacion con google',
+      items: [
+        { name: 'Actividad 1', completed: true},
+        { name: 'Actividad 2', completed: false},
+        { name: 'Actividad 3', completed: false}
+      ]
+
+    },
+    {
+      id: "2",
+      title: 'Autenticacion con google',
+      description: 'Esta es una tarea de autenticacion con google',
+      items: [
+        { name: 'Actividad 1', completed: true},
+        { name: 'Actividad 2', completed: false},
+        { name: 'Actividad 3', completed: false}
+      ]
+
+    },
+    {
+      id: "3",
+      title: 'Autenticacion con google',
+      description: 'Esta es una tarea de autenticacion con google',
+      items: [
+        { name: 'Actividad 1', completed: true},
+        { name: 'Actividad 2', completed: false},
+        { name: 'Actividad 3', completed: false}
+      ]
+
+    },
+  ]
 
   constructor() { }
 
